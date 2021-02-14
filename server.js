aa2cbef6a84aa039752b82c839e5278deaa4661b
frankmagoba <franklinmagoba4@gmail.com>
2021-02-14 22:14:59 +0300

    installed mongoose

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser= require('body-parser')
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3000, function() {
     console.log('listening on 3000')
@@ -10,5 +13,4 @@ app.listen(3000, function() {
   })
 
   app.post('/album', (req, res) => {
-    console.log('Hellooooooooooooooooo!')
-  })
+    console.log(req.body)  })
