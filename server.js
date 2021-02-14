5e01825a92bfc312f71d8cd9f8d59a8154d3ba83
frankmagoba <franklinmagoba4@gmail.com>
2021-02-14 23:05:17 +0300

    crud album operations with mongo db

--- server.js
+++ server.js
@@ -1,16 +1,82 @@
 const express = require('express');
 const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+const connectionString = "mongodb+srv://lin:icui4cu2@cluster0.eq9ns.mongodb.net/albums?retryWrites=true&w=majority"
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(3000, function() {
-    console.log('listening on 3000')
-  })
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('albums')
+    const albumCollection = db.collection('albums')
 
-  app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-  })
+    // ========================
+    // Middlewares
+    // ========================
+    app.set('view engine', 'ejs')
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
+    app.use(express.static('public'))
 
-  app.post('/album', (req, res) => {
-    console.log(req.body)  })
+    // ========================
+    // Routes
+    // ========================
+    app.get('/', (req, res) => {
+      db.collection('albums').find().toArray()
+        .then(albums => {
+          res.render('index.ejs', { albums: albums })
+        })
+        .catch(/* ... */)
+    })
+
+    app.post('/albums', (req, res) => {
+      albumCollection.insertOne(req.body)
+        .then(result => {
+          res.redirect('/')
+        })
+        .catch(error => console.error(error))
+    })
+
+    app.put('/albums', (req, res) => {
+      albumCollection.findOneAndUpdate(
+        { name: 'Yoda' },
+        {
+          $set: {
+            name: req.body.name,
+            album: req.body.album
+          }
+        },
+        {
+          upsert: true
+        }
+      )
+        .then(result => res.json('Success'))
+        .catch(error => console.error(error))
+    })
+
+    app.delete('/albums', (req, res) => {
+      albumCollection.deleteOne(
+        { name: req.body.name }
+      )
+        .then(result => {
+          if (result.deletedCount === 0) {
+            return res.json('No album to delete')
+          }
+          res.json('Deleted Darth Vadar\'s album')
+        })
+        .catch(error => console.error(error))
+    })
+
+    // ========================
+    // Listen
+    // ========================
+    const port = 3000
+    app.listen(port, function () {
+      console.log(`listening on ${port}`)
+    })
+  })
+  .catch(console.error)
