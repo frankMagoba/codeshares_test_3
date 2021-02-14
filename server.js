a75ea448a0c78b87b999aad3ef4d8ec8520dca5e
frankmagoba <franklinmagoba4@gmail.com>
2021-02-14 21:53:48 +0300

    installed nodemon

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('May Node be with you')
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+    console.log('listening on 3000')
+  })
+
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+    // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+  })
